cd314a8f0a5ed6ca6a3510d23815fd1b4772c458
Petrov Aleksandr <rpm10b@gmail.com>
2020-10-01 17:29:51 +0300

    Updates

---
+++ precache-manifest.b3e7206f64518df9c4456bf8b4bdf8ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e274497db6e75bf5e7b8509ce23d9ead",
+    "url": "/client__Registrar/index.html"
+  },
+  {
+    "revision": "9a65a9c7e999151fc84d",
+    "url": "/client__Registrar/static/css/main.0a77d734.chunk.css"
+  },
+  {
+    "revision": "0205f62f0e6d43662f79",
+    "url": "/client__Registrar/static/js/2.b6723960.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/client__Registrar/static/js/2.b6723960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a65a9c7e999151fc84d",
+    "url": "/client__Registrar/static/js/main.b9ee1883.chunk.js"
+  },
+  {
+    "revision": "74279bccbe51e153226d",
+    "url": "/client__Registrar/static/js/runtime-main.5b5f335c.js"
+  },
+  {
+    "revision": "4f99f1479d275907000a6537796a5809",
+    "url": "/client__Registrar/static/media/avatar.4f99f147.jpg"
+  }
+]);
